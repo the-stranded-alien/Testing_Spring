30c67ea3a976bbda4d877f2c70768fed2bebc116
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-27 23:37:11 +0530

    JUnit 5 Yanny and Laurel Test

--- src/test/java/org/springframework/samples/petclinic/yannylaurel/BaseConfig.java
+++ src/test/java/org/springframework/samples/petclinic/yannylaurel/BaseConfig.java
@@ -1,5 +1,6 @@
 package org.springframework.samples.petclinic.yannylaurel;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
---
+++ src/test/java/org/springframework/samples/petclinic/yannylaurel/junit5/HearingInterpreterLaurelTest.java
@@ -0,0 +1,23 @@
+package org.springframework.samples.petclinic.yannylaurel.junit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.yannylaurel.BaseConfig;
+import org.springframework.samples.petclinic.yannylaurel.HearingInterpreter;
+import org.springframework.samples.petclinic.yannylaurel.LaurelConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfig.class})
+class HearingInterpreterLaurelTest {
+
+    @Autowired
+    HearingInterpreter hearingInterpreter;
+
+    @Test
+    void whatIHeard() {
+        String word = hearingInterpreter.whatIHeard();
+        assertEquals("Laurel", word);
+    }
+}
