f6bf944b77c7c5491dff1258414a922fd82fd36e
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-12-28 12:42:04 +0530

    Spring Web Configuration with XML

---
+++ src/test/java/org/springframework/samples/petclinic/web/OwnerControllerTest.java
@@ -0,0 +1,25 @@
+package org.springframework.samples.petclinic.web;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.service.ClinicService;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitWebConfig(locations = {"classpath:spring/mvc-test-config.xml", "classpath:spring/mvc-core-config.xml"})
+class OwnerControllerTest {
+
+    @Autowired
+    OwnerController ownerController;
+
+    @Autowired
+    ClinicService clinicService;
+
+    @Test
+    void tempTest() {
+
+    }
+}
